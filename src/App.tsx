eb8133e30ce54715abb7ceacd9539871cf0174e5
FlyBoy292 <r.benetcorbin@gmail.com>
2026-07-06 15:37:41 +0200

    feat(ui): create initial NeoFMS landing page

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+export default function App() {
+    return (
+        <main>
+            <h1>NeoFMS</h1>
+
+            <p>Learn the system.</p>
+
+            <p>Not the buttons.</p>
+
+            <button>Start Academy</button>
+
+            <button>Open MCDU</button>
+        </main>
+    );
+}
